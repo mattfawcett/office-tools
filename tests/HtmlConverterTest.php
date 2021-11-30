1b21399341b144e0f9251948576eeda53aee38bb
Matt Fawcett <matt.fawcett@formstack.com>
2021-11-30 13:41:11 +0000

    Add a test for header/footer in subtables

--- tests/HtmlConverterTest.php
+++ tests/HtmlConverterTest.php
@@ -8,8 +8,26 @@ class HtmlConverterTest extends TestCase
     /** @dataProvider htmlConverterImplementations **/
     public function testConvertWhenHeaderAndFooterInSingleTable(HtmlConverter $converter)
     {
+        $this->markTestSkipped('Newer office does not seem to like both header and footer in single table');
         $inputPath = $this->inputFilePath('header-footer-in-single-table.html');
         $outputPath = $this->outputFilePath($converter, 'header-footer-in-single-table.html.docx');
+
+        if ($this->shouldRegenerate($outputPath)) {
+            $converter->convertToWord($inputPath, $outputPath);
+        }
+
+        $zip = $this->assertZip($outputPath);
+        $fileIndex = $this->assertZipHasFileNamed($zip, 'word/document.xml');
+
+        $document = $zip->getFromIndex($fileIndex);
+        $this->assertStringContainsString('<w:headerReference', $document);
+    }
+
+    /** @dataProvider htmlConverterImplementations **/
+    public function testConvertWhenHeaderAndFooterInSubTables(HtmlConverter $converter)
+    {
+        $inputPath = $this->inputFilePath('header-footer-in-sub-tables.html');
+        $outputPath = $this->outputFilePath($converter, 'header-footer-in-sub-tables.html.docx');
 
         if ($this->shouldRegenerate($outputPath)) {
             $converter->convertToWord($inputPath, $outputPath);
