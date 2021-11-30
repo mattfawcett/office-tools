1be5cbe11dd924819d26c1a2001348c6cce41b48
Matt Fawcett <matt.fawcett@formstack.com>
2021-11-30 16:14:05 +0000

    Add test to show html to docx working

--- tests/HtmlConverterTest.php
+++ tests/HtmlConverterTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests;
 
+use WebmergeOfficeTools\Factory;
 use WebmergeOfficeTools\HtmlConverter;
 
 class HtmlConverterTest extends TestCase
@@ -38,5 +39,24 @@ class HtmlConverterTest extends TestCase
 
         $document = $zip->getFromIndex($fileIndex);
         $this->assertStringContainsString('<w:headerReference', $document);
+
+        $docxHash = sha1_file($outputPath);
+        $pdfPath = $this->outputFilePath($converter, "header-footer-in-sub-tables.html.docx.$docxHash.pdf");
+        if (!file_exists($pdfPath)) {
+            $this->convertToPdf($outputPath, $pdfPath);
+        }
+
+        $png = $this->toPng($pdfPath);
+
+        $benchmark = $this->toPng($this->benchmarkFilePath('header-footer-in-sub-tables.html.docx.pdf'));
+
+        $this->assertImagesSimilar($benchmark, $png);
+    }
+
+    private function convertToPdf(string $docxPath, string $pdfPath): void
+    {
+        $converter = Factory::wordConverter(Factory::SYSTEM_CONVERTAPI_DOT_COM);
+
+        $converter->convertToPdf($docxPath, $pdfPath);
     }
 }
