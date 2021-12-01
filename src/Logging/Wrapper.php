cddfabd03a9f841ae0cd0c2336af0a0f7cc92188
Matt Fawcett <matt.fawcett@formstack.com>
2021-12-01 10:25:06 +0000

    Start adding more logging

--- src/Logging/Wrapper.php
+++ src/Logging/Wrapper.php
@@ -3,6 +3,7 @@ namespace WebmergeOfficeTools\Logging;
 
 use RuntimeException;
 use WebmergeOfficeTools\Configuration;
+use WebmergeOfficeTools\Exceptions\Exception;
 use WebmergeOfficeTools\WordConverter;
 
 class Wrapper implements WordConverter
@@ -29,17 +30,44 @@ class Wrapper implements WordConverter
         $this->assertBaseImplementationIs(WordConverter::class);
         assert($this->baseImplementation instanceof WordConverter);
 
+        $this->withLogging(
+            fn() => $this->baseImplementation->convertWordToPdf($filePath, $outputFilePath),
+            "Converting word file to pdf",
+            [
+                'inputFilePath' => $filePath,
+                'outputFilePath' => $outputFilePath,
+            ]
+        );
+    }
+
+    private function withLogging(callable $run, string $message, array $additionalData): void
+    {
+         $debugBacktrace = array_map(function ($trace) {
+             return [
+                 'file' => $trace['file'] ?? null,
+                 'line' => $trace['line'] ?? null,
+                 'function' => $trace['function'] ?? null,
+             ];
+         }, array_slice(debug_backtrace(), 0, 5));
+
+
+        $startTimeMicroSeconds = microtime(true);
         try {
-            $this->baseImplementation->convertWordToPdf($filePath, $outputFilePath);
-            Configuration::logger()->info('Converted word file to pdf', [
-                'inputFilePath' => $filePath,
-                'outputFilePath' => $outputFilePath,
-            ]);
-        } catch (\Exception $e) {
-            Configuration::logger()->error('Error converted word file to pdf', [
-                'inputFilePath' => $filePath,
-                'outputFilePath' => $outputFilePath,
-            ]);
+            $run();
+            $deltaTimeMilliseconds = intval(1000 * (microtime(true) - $startTimeMicroSeconds));
+            Configuration::logger()->info("$message: success", array_merge($additionalData, [
+                'deltaTimeMilliseconds' => $deltaTimeMilliseconds,
+                'implementationName' => $this->baseImplementation->implementationName(),
+                'debug_backtrace' => $debugBacktrace,
+            ]));
+        } catch (Exception $e) {
+            $deltaTimeMilliseconds = intval(1000 * (microtime(true) - $startTimeMicroSeconds));
+            Configuration::logger()->error("$message: failure", array_merge($additionalData, [
+                'deltaTimeMilliseconds' => $deltaTimeMilliseconds,
+                'implementationName' => $this->baseImplementation->implementationName(),
+                'debug_backtrace' => $debugBacktrace,
+            ]));
+
             throw $e;
         }
     }
